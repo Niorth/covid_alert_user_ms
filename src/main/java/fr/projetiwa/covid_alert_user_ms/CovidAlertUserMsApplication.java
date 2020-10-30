5114a7229ec7f4d507482fdb16fdc10479642421
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-10-30 16:25:05 +0100

    init ms user

---
+++ src/main/java/fr/projetiwa/covid_alert_user_ms/CovidAlertUserMsApplication.java
@@ -0,0 +1,13 @@
+package fr.projetiwa.covid_alert_user_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAlertUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAlertUserMsApplication.class, args);
+	}
+
+}
